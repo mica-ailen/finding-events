1fc035519938c0c9f10ab20a7b6ce4a838cc182a
Mic <you@example.com>
2025-11-05 16:16:12 +0100

    extracting more info

--- find-events.py
+++ find-events.py
@@ -6,21 +6,34 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 # URL to scrape
-url = "https://www.visitcopenhagen.com/explore/events-cid58/events-cid59?filters=region:2;date-range:[2025-10-30T00:00:00.000Z%20TO%202025-12-31T23:59:59.999Z]"
+url = "https://www.visitcopenhagen.com/explore/events-cid58/events-cid59?filters=region:2"
 
 driver = webdriver.Chrome()
+events = []
 
 try:
     driver.get(url)
     
-    element = WebDriverWait(driver, 10).until(
+    element = WebDriverWait(driver, 12).until(
         EC.element_to_be_clickable((By.ID, "declineButton"))
     )
     element.click()  
     print("Cookie declined!")
-    
-    title = driver.find_element(By.CLASS_NAME, "name")
-    print(title.text)
+
+    driver.implicitly_wait(10)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+    event_items = driver.find_elements(By.XPATH, "//div[@class='sc-bb566763-28 gEMIVp xs-12 md-4']")
+    print(len(event_items))
+
+    for event_item in event_items:
+        name = event_item.find_element(By.XPATH, ".//h3[@class='sc-2a579a92-24 bZVIvy']").text
+        dates = event_item.find_element(By.XPATH,".//div[@class='sc-2a579a92-26 EPKGt']").text
+        url = event_item.find_element(By.XPATH, ".//a[@class='link-internal']").get_attribute('href')
+
+        print(f"Name: {name}")
+        print(f"Dates: {dates}")
+        print(f"Link: {url}")
     
     
 except Exception as e:
